f328d48143b7f447b39761e123f0ce17816df747
AbelCodeCanvas <abelmhlanga40@gmail.com>
2025-12-03 12:32:51 +0200

    Set up Apollo Server with GraphQL schema

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,21 @@
-test
+import { ApolloServer } from 'apollo-server';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { rankingResolver } from './resolvers/rankingResolver';
+
+const typeDefs = readFileSync(
+  join(__dirname, 'schema/schema.graphql'),
+  'utf-8'
+);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: rankingResolver,
+  context: ({ req }) => ({
+    // You could add authentication context here
+  }),
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
